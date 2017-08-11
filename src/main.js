b304485961ae9b55df683e09887dcbb744afefcc
Christophe Coquelet <coquelet.c@gmail.com>
2017-08-12 01:58:05 +0200

    define prototype and add application

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import LowPoly from './LowPoly';
 import DOM from './DOM';
+import Application from './Application';
+
+Array.prototype.clone = function () {
+    return this.slice(0);
+}
 
 let DOMObject = new DOM().initialize();
+let App = new Application(DOMObject)
 let lowPoly = new LowPoly(DOMObject.get('origin'), DOMObject.get('render'));
 
 DOMObject.get('import').addEventListener("change", lowPoly.importImage.bind(lowPoly));
